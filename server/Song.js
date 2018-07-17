555f6d8a5338504fca9f5a36f3f8c8a9c745723b
Joey Tepperman <jmichaelt98@gmail.com>
2018-07-16 21:52:37 -0400

    Add cap on DB

--- server/Song.js
+++ server/Song.js
@@ -1,43 +1,46 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const schema = new Schema({
-  _id: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true
+const schema = new Schema(
+  {
+    _id: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true
+    },
+    song: {
+      type: String,
+      required: true
+    },
+    artist: {
+      type: String,
+      required: true
+    },
+    album: {
+      type: String,
+      required: true
+    },
+    url: {
+      type: String,
+      required: true
+    },
+    art: {
+      type: String,
+      required: true
+    },
+    likes: {
+      type: Number,
+      required: true
+    },
+    dislikes: {
+      type: Number,
+      required: true
+    },
+    verdict: {
+      type: Number,
+      required: true
+    }
   },
-  song: {
-    type: String,
-    required: true
-  },
-  artist: {
-    type: String,
-    required: true
-  },
-  album: {
-    type: String,
-    required: true
-  },
-  url: {
-    type: String,
-    required: true
-  },
-  art: {
-    type: String,
-    required: true
-  },
-  likes: {
-    type: Number,
-    required: true
-  },
-  dislikes: {
-    type: Number,
-    required: true
-  },
-  verdict: {
-    type: Number,
-    required: true
-  }
-});
+  { capped: 1000 }
+);
 
 module.exports = mongoose.model("Song", schema);
